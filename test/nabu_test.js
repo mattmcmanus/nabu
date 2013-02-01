7b04c21c5a44cb6e432008d6e26d5c9fd9a109fa
Matt McManus <mattmcmanus@gmail.com>
2013-01-31 22:45:46 -0500

    Inital Commit

---
+++ test/nabu_test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var nabu = require('../lib/nabu.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['loadFiles'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(1);
+    // tests here
+    var site = nabu.loadFiles();
+    console.log(site);
+    test.ok(site,'should be awesome.');
+    test.done();
+  },
+};
