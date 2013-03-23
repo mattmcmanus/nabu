e67b7c004bb08bbeeda87ff2d21e797ee49cad38
Matt McManus <mattmcmanus@gmail.com>
2013-03-23 18:23:35 -0400

    Full site generation! Reworked tests and cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,7 @@ module.exports = function(grunt) {
     nodeunit: {
       files: ['test/**/*_test.js'],
     },
+
     jshint: {
       options: {
         jshintrc: '.jshintrc'
@@ -14,33 +15,44 @@ module.exports = function(grunt) {
       gruntfile: {
         src: 'Gruntfile.js'
       },
+      nabu_modules: {
+        src: 'node_modules/nabu-*/*.js'
+      },
       lib: {
-	src: ['lib/**/*.js', 'bin/*']
+        src: ['lib/**/*.js', 'bin/*']
       },
       test: {
         src: ['test/**/*.js']
       },
     },
+
     watch: {
       gruntfile: {
         files: '<%= jshint.gruntfile.src %>',
         tasks: ['jshint:gruntfile']
       },
+      nabu_modules: {
+        files: '<%= jshint.nabu_modules.src %>',
+        tasks: ['jshint:nabu_modules', 'nodeunit', 'clean']
+      },
       lib: {
         files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
+        tasks: ['jshint:lib', 'nodeunit', 'clean']
       },
       test: {
         files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
-      },
+        tasks: ['jshint:test', 'nodeunit', 'clean']
+      }
     },
+
+    clean: ['test/fixtures/_site'],
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'nodeunit']);
--- test/nabu_test.js
+++ test/nabu_test.js
@@ -1,8 +1,6 @@
 'use strict';
 
-var nabu = require('../lib/nabu.js'),
-    fs = require('fs'),
-    rimraf = require('rimraf'),
+var fs = require('fs'),
     path = require('path');
 
 /*
@@ -27,42 +25,71 @@ var nabu = require('../lib/nabu.js'),
 
 // var site = {};
 
-exports['nabu'] = {
+process.chdir('./test/fixtures');
+
+// var nabu;
+
+// exports['nabu'] = {
+//   setUp: function(done) {
+//     nabu = require('../lib/nabu.js');
+//     done();
+//   },
+
+//   'bootstrap()': function(test) {
+//     test.expect(2);
+//     // tests here
+//     nabu.bootstrap();
+
+//     test.ok(nabu,'There should be a nabu object');
+//     test.equal(nabu.site.destination, './_site', 'Destination dir should be _site');
+
+//     test.done();
+//   },
+
+//   'loadFiles()': function(test) {
+//     test.expect(2);
+    
+//     nabu.loadFiles();
+//     test.ok((nabu._files.length > 1));
+//     test.ok((nabu._files.indexOf('./_config.json') === -1), 'It should ignore the config file');
+//     test.done();
+//   },
+
+//   'parseFiles()': function(test) {
+//     test.expect(2);
+    
+//     nabu.parseFiles(function(err){
+//       test.ok(nabu, 'There shold be a nabu object');
+//       test.ok(nabu.site.assets, 'There should be at least 1 asset');
+//       test.done();
+//     });
+//   },
+
+//   tearDown: function(done) {
+//     nabu = null;
+//     done()
+//   }
+// };
+
+
+var nabu2;
+
+exports['commands'] = {
   setUp: function(done) {
+    nabu2 = require('../lib/nabu.js');
     done();
   },
-
-  'bootstrap': function(test) {
+  'generate()': function(test) {
     test.expect(2);
-    // tests here
-    nabu.bootstrap('./test/fixtures');
-
-    test.ok(nabu,'There should be a nabu object');
-    test.equal(nabu.site.destination, './_site', 'Destination dir should be _site');
-
-    test.done();
-  },
-
-  'loadFiles': function(test) {
-    test.expect(2);
-    
-    nabu.loadFiles();
-    test.ok((nabu._files.length > 1));
-    test.ok((nabu._files.indexOf('./_config.json') === -1), "It should ignore the config file");
-    test.done();
-  },
-
-  'parseFiles': function(test) {
-    test.expect(2);
-    
-    nabu.parseFiles(function(err){
-      test.ok(nabu, "There shold be a nabu object");
-      test.ok(nabu.site.assets, "There should be at least 1 asset");
+    nabu2.generate(function(err, result){
+      test.equal(err, null, 'There should be no returned errors');
+      test.equal(result, 0, 'The result of generate should be an exit code of 0');
       test.done();
     });
   },
 
   tearDown: function(done) {
-    rimraf('_site', done);
+    nabu2 = null;
+    done();
   }
 };
