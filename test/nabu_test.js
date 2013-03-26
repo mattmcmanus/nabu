1e0cf7e1643f34fac0dea1c816338ffee9e3666a
Matt McManus <mattmcmanus@gmail.com>
2013-03-25 22:39:04 -0400

    Nabu test now works. None of the plugin tests do….

--- test/nabu_test.js
+++ test/nabu_test.js
@@ -27,71 +27,62 @@ var fs = require('fs'),
 
 process.chdir('./test/fixtures');
 
-// var nabu;
+var nabu = require('../lib/nabu.js');
+var generator;
 
-// exports['nabu'] = {
-//   setUp: function(done) {
-//     nabu = require('../lib/nabu.js');
-//     done();
-//   },
-
-//   'bootstrap()': function(test) {
-//     test.expect(2);
-//     // tests here
-//     nabu.bootstrap();
-
-//     test.ok(nabu,'There should be a nabu object');
-//     test.equal(nabu.site.destination, './_site', 'Destination dir should be _site');
-
-//     test.done();
-//   },
-
-//   'loadFiles()': function(test) {
-//     test.expect(2);
-    
-//     nabu.loadFiles();
-//     test.ok((nabu._files.length > 1));
-//     test.ok((nabu._files.indexOf('./_config.json') === -1), 'It should ignore the config file');
-//     test.done();
-//   },
-
-//   'parseFiles()': function(test) {
-//     test.expect(2);
-    
-//     nabu.parseFiles(function(err){
-//       test.ok(nabu, 'There shold be a nabu object');
-//       test.ok(nabu.site.assets, 'There should be at least 1 asset');
-//       test.done();
-//     });
-//   },
-
-//   tearDown: function(done) {
-//     nabu = null;
-//     done()
-//   }
-// };
-
-
-var nabu2, generator;
-
-exports['commands'] = {
+exports['nabu'] = {
   setUp: function(done) {
-    nabu2 = require('../lib/nabu.js');
-    generator = nabu2();
-    console.log(generator);
+    generator = nabu();
     done();
   },
-  'generate()': function(test) {
+
+  'loadFiles()': function(test) {
     test.expect(2);
-    generator.generate(function(err, result){
-      test.equal(err, null, 'There should be no returned errors');
-      test.equal(result, 0, 'The result of generate should be an exit code of 0');
+    
+    generator.loadFiles();
+    // console.log(generator._files);
+    test.ok((generator._files.length > 1));
+    test.ok((generator._files.indexOf('./_config.json') === -1), 'It should ignore the config file');
+    test.done();
+  },
+
+  'parseFiles()': function(test) {
+    test.expect(3);
+    generator.loadFiles();
+    generator.parseFiles(function(err, results){
+      // console.log(generator);
+      test.ok(generator, 'There shold be a nabu object');
+      test.equal(err, null, 'err should be null');
+      test.equal(generator.site.assets.length, 2, 'There should be at least 1 asset');
       test.done();
     });
   },
 
   tearDown: function(done) {
-    nabu2 = null;
+    generator = null;
     done();
   }
 };
+
+
+// var generator2;
+
+// exports['commands'] = {
+//   setUp: function(done) {
+//     generator2 = nabu();
+//     done();
+//   },
+//   'generate()': function(test) {
+//     test.expect(2);
+//     generator2.generate(function(err, result){
+//       test.equal(err, null, 'There should be no returned errors');
+//       test.equal(result, 0, 'The result of generate should be an exit code of 0');
+//       test.done();
+//     });
+//   },
+
+//   tearDown: function(done) {
+//     generator2 = null;
+//     done();
+//   }
+// };
